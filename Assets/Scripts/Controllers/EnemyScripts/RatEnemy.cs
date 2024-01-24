bd482b3e8f38b30a8424be9bb34f975d961aff01
ErikRivadeneira <erik.rivadeneira@udla.edu.ec>
2024-01-24 14:36:35 -0500

    - Added UI Scaling with screen size

--- Assets/Scripts/Controllers/EnemyScripts/RatEnemy.cs
+++ Assets/Scripts/Controllers/EnemyScripts/RatEnemy.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
-using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class RatEnemy : MonoBehaviour
 {
